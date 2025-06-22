96a925e9d8390138a0251d034cea9b50e847a718
AminaHanan246 <aminahanaan0310@gmail.com>
2025-06-22 21:49:38 +0530

    QC and filtering

--- prj_GSE272922.R
+++ prj_GSE272922.R
@@ -64,4 +64,31 @@ split_id = do.call(rbind,strsplit(data_merged$orig.ident,"_"))
 data_merged$geo_id = split_id[,1]
 data_merged$treatment = split_id[,2]
 
+#===========================================
+#QC and filtering https://www.sc-best-practices.org/preprocessing_visualization/quality_control.html
+#===========================================
 
+#low quality bases - high mit contamination
+data_merged[["percent.mt"]] <- PercentageFeatureSet(data_merged, pattern = "^mt-") #mouse is ^mt-
+
+sum(grepl("^mt-", rownames(data_merged)))
+head(rownames(data_merged), 20)
+
+
+VlnPlot(data_merged, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+FeatureScatter(data_merged, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+
+quantile(data_merged$nFeature_RNA, probs = c(0.01, 0.99))
+quantile(data_merged$nCount_RNA, probs = c(0.01, 0.99))
+quantile(data_merged$percent.mt, probs = c(0.01, 0.99))
+
+
+data_filtered <- subset(
+  data_merged,
+  subset = nFeature_RNA > 500 & nFeature_RNA < 7500 & percent.mt < 10)
+
+# Visualize QC metrics as a violin plot
+VlnPlot(data_filtered, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+nrow(data_filtered)           # total number of genes/features
+sum(grepl("^mt-", rownames(data_filtered)))  # how many are mitochondrial
